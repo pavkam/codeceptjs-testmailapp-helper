03e669d793be84897a1c251e2b2bfde3c3cb830d
Alexandru Ciobanu <alex+git@ciobanu.org>
2020-11-04 10:58:51 +0000

    style: added prettier

--- index.js
+++ index.js
@@ -27,20 +27,22 @@ const { GraphQLClient } = require('@testmail.app/graphql-request');
 /* Random function to generate the tags.*/
 const randomCharSet = '0123456789abcdefghijklmnopqrstuvwxyz';
 const randomString = (length) => {
-    let result = '';
-    for (let i = length; i > 0; --i) {
-        result += randomCharSet[Math.floor(Math.random() * randomCharSet.length)];
-    }
-    return result;
-}
+  let result = '';
+  for (let i = length; i > 0; --i) {
+    result += randomCharSet[Math.floor(Math.random() * randomCharSet.length)];
+  }
+  return result;
+};
 
 /* Quick-and-dirty sleep promise */
 const sleep = (ms) => {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
 const msInSecond = 1000;
-let emailPattern = new RegExp('([a-zA-Z0-9]+)\.([a-zA-Z0-9]+)@inbox\.testmail\.app');
+let emailPattern = new RegExp(
+  '([a-zA-Z0-9]+).([a-zA-Z0-9]+)@inbox.testmail.app'
+);
 
 /**
  * Adds support for end-to-end email testing using [Testmail.app](https://testmail.app/).
@@ -69,116 +71,121 @@ let emailPattern = new RegExp('([a-zA-Z0-9]+)\.([a-zA-Z0-9]+)@inbox\.testmail\.a
  *
  */
 class TestMailAppInboxHelper extends Helper {
+  constructor(config) {
+    super(config);
 
-    constructor(config) {
-        super(config);
+    this.sleepDelay =
+      (this.config.sleepDelay ? Number(this.config.sleepDelay) : 5) *
+      msInSecond;
+    this.tagLength = this.config.tagLength ? Number(this.config.tagLength) : 8;
+    this.defaultTimeout =
+      (this.config.defaultTimeout ? Number(this.config.defaultTimeout) : 240) *
+      msInSecond;
 
-        this.sleepDelay = (this.config.sleepDelay ? Number(this.config.sleepDelay) : 5) * msInSecond;
-        this.tagLength = this.config.tagLength ? Number(this.config.tagLength) : 8;
-        this.defaultTimeout = (this.config.defaultTimeout ? Number(this.config.defaultTimeout) : 240) * msInSecond;
-
-        if (!this.config.apiKey) {
-            throw new Error('`apiKey` configuration property is not set.');
-        }
-
-        if (!this.config.namespace) {
-            throw new Error('`namespace` configuration property is not set.');
-        }
-
-        this.client = new GraphQLClient('https://api.testmail.app/api/graphql', {
-            headers: { 'Authorization': `Bearer ${this.config.apiKey}` }
-        });
+    if (!this.config.apiKey) {
+      throw new Error('`apiKey` configuration property is not set.');
     }
 
-    /**
-     * Defines/creates a new inbox by generating a new tag.
-     * If `email` argument is supplied, it is used to create the inbox. The format of the email
-     * should be as follows: `namespace.tag@inbox.testmail.app`.
-     *
-     * The function returns the new inbox and also sets it as "current".
-     *
-     * ```js
-     * const inbox = await I.haveInbox();
-     * ```
-     * or ...
-     * ```js
-     * const inbox = await I.haveInbox("abcde.123456789@inbox.testmail.app");
-     * ```
-     *
-     * @param {string?} email to  create the inbox
-     *
-     */
-    async haveInbox(email) {
-
-        let tag = randomString(this.tagLength);
-        let namespace = this.config.namespace;
-
-        if (email) {
-            const match = emailPattern.exec(email);
-            if (match && match.length == 3) {
-                namespace = match[1];
-                tag = match[2];
-            } else {
-                throw new Error("Invalid email format supplied (must be namespace.tag@inbox.testmail.app).");
-            }
-        }
-
-        email = `${namespace}.${tag}@inbox.testmail.app`;
-        this.lastUsedInbox = {
-            tag: tag,
-            namespace: namespace,
-            timestamp: Date.now(),
-            email: email,
-            toString: () => {
-                return email;
-            }
-        }
-
-        return this.lastUsedInbox;
+    if (!this.config.namespace) {
+      throw new Error('`namespace` configuration property is not set.');
     }
 
-    /**
-     * Receives all new emails since last call to `receiveEmails` for the given inbox.
-     * If `timeout` argument is supplied, it will override the default timeout (in seconds).
-     * If the `inbox` argument is not supplied, the last created inbox will be used.
-     *
-     * If no emails are received, an error is raised.
-     *
-     * ```js
-     * const emails = await I.receiveEmails();
-     * ```
-     * or ...
-     * ```js
-     * const emails = await I.receiveEmails(inbox, 100);
-     * ```
-     *
-     * @param {*} inbox
-     * @param {number?} timeout
-     *
-     */
-    async receiveEmails(inbox, timeout) {
+    this.client = new GraphQLClient('https://api.testmail.app/api/graphql', {
+      headers: { 'Authorization': `Bearer ${this.config.apiKey}` }
+    });
+  }
 
-        /* Prepare the timeout */
-        if (!timeout) {
-            timeout = this.defaultTimeout;
-        } else {
-            timeout *= msInSecond;
-        }
+  /**
+   * Defines/creates a new inbox by generating a new tag.
+   * If `email` argument is supplied, it is used to create the inbox. The format of the email
+   * should be as follows: `namespace.tag@inbox.testmail.app`.
+   *
+   * The function returns the new inbox and also sets it as "current".
+   *
+   * ```js
+   * const inbox = await I.haveInbox();
+   * ```
+   * or ...
+   * ```js
+   * const inbox = await I.haveInbox("abcde.123456789@inbox.testmail.app");
+   * ```
+   *
+   * @param {string?} email to  create the inbox
+   *
+   */
+  async haveInbox(email) {
+    let tag = randomString(this.tagLength);
+    let namespace = this.config.namespace;
 
-        /* Prepare the inbox */
-        if (!inbox) {
-            inbox = this.lastUsedInbox;
-        }
+    if (email) {
+      const match = emailPattern.exec(email);
+      if (match && match.length == 3) {
+        namespace = match[1];
+        tag = match[2];
+      } else {
+        throw new Error(
+          'Invalid email format supplied (must be namespace.tag@inbox.testmail.app).'
+        );
+      }
+    }
 
-        if (!inbox || !inbox.tag || !inbox.namespace || !inbox.timestamp) {
-            throw new Error("No/invalid inbox argument supplied and no previous inbox has been opened.");
-        }
+    email = `${namespace}.${tag}@inbox.testmail.app`;
+    this.lastUsedInbox = {
+      tag: tag,
+      namespace: namespace,
+      timestamp: Date.now(),
+      email: email,
+      toString: () => {
+        return email;
+      }
+    };
 
-        while (timeout > 0) {
-            await sleep(this.sleepDelay);
-            timeout -= this.sleepDelay;
+    return this.lastUsedInbox;
+  }
 
-            const data = await this.client.request(`{
+  /**
+   * Receives all new emails since last call to `receiveEmails` for the given inbox.
+   * If `timeout` argument is supplied, it will override the default timeout (in seconds).
+   * If the `inbox` argument is not supplied, the last created inbox will be used.
+   *
+   * If no emails are received, an error is raised.
+   *
+   * ```js
+   * const emails = await I.receiveEmails();
+   * ```
+   * or ...
+   * ```js
+   * const emails = await I.receiveEmails(inbox, 100);
+   * ```
+   *
+   * @param {*} inbox
+   * @param {number?} timeout
+   *
+   */
+  async receiveEmails(inbox, timeout) {
+    /* Prepare the timeout */
+    if (!timeout) {
+      timeout = this.defaultTimeout;
+    } else {
+      timeout *= msInSecond;
+    }
+
+    /* Prepare the inbox */
+    if (!inbox) {
+      inbox = this.lastUsedInbox;
+    }
+
+    if (!inbox || !inbox.tag || !inbox.namespace || !inbox.timestamp) {
+      throw new Error(
+        'No/invalid inbox argument supplied and no previous inbox has been opened.'
+      );
+    }
+
+    while (timeout > 0) {
+      await sleep(this.sleepDelay);
+      timeout -= this.sleepDelay;
+
+      const data = await this.client.request(`{
                 inbox (
                     namespace:"${inbox.namespace}"
                     tag:"${inbox.tag}"
@@ -195,38 +202,44 @@ class TestMailAppInboxHelper extends Helper {
                 }
             }`);
 
-            if (data && data.inbox && data.inbox.result === "success" && data.inbox.emails && data.inbox.emails.length > 0) {
-                inbox.timestamp = Date.now();
-                return data.inbox.emails;
-            }
-        }
-
-        throw new Error(`Did not receive any new email message.`);
+      if (
+        data &&
+        data.inbox &&
+        data.inbox.result === 'success' &&
+        data.inbox.emails &&
+        data.inbox.emails.length > 0
+      ) {
+        inbox.timestamp = Date.now();
+        return data.inbox.emails;
+      }
     }
 
-    /**
-     * Receives all the first new email since last call to `receiveEmail` for the given inbox.
-     * If `timeout` argument is supplied, it will override the default timeout (in seconds).
-     * If the `inbox` argument is not supplied, the last created inbox will be used.
-     *
-     * If no emails are received, an error is raised.
-     *
-     * ```js
-     * const email = await I.receiveEmail();
-     * ```
-     * or ...
-     * ```js
-     * const email = await I.receiveEmail(inbox, 100);
-     * ```
-     *
-     * @param {*} inbox
-     * @param {number?} timeout
-     *
-     */
-    async receiveEmail(inbox, timeout) {
-        const emails = await this.receiveEmails(inbox, timeout);
-        return emails[0];
-    }
+    throw new Error(`Did not receive any new email message.`);
+  }
+
+  /**
+   * Receives all the first new email since last call to `receiveEmail` for the given inbox.
+   * If `timeout` argument is supplied, it will override the default timeout (in seconds).
+   * If the `inbox` argument is not supplied, the last created inbox will be used.
+   *
+   * If no emails are received, an error is raised.
+   *
+   * ```js
+   * const email = await I.receiveEmail();
+   * ```
+   * or ...
+   * ```js
+   * const email = await I.receiveEmail(inbox, 100);
+   * ```
+   *
+   * @param {*} inbox
+   * @param {number?} timeout
+   *
+   */
+  async receiveEmail(inbox, timeout) {
+    const emails = await this.receiveEmails(inbox, timeout);
+    return emails[0];
+  }
 }
 
 module.exports = TestMailAppInboxHelper;
